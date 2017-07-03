b8ecadf4fe4750900493c3f077c9b16243945e0d
YukiTakamura <g1527047@st.t-kougei.ac.jp>
2017-07-03 11:09:31 +0900

    DLLプロジェクト作成

---
+++ DLL_Sugoroku/DLL_Sugoroku/DLL_Sugoroku.cpp
@@ -0,0 +1,27 @@
+// DLL_Sugoroku.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+#include "DLL_Sugoroku.h"
+
+
+// This is an example of an exported variable
+DLL_SUGOROKU_API int nDLL_Sugoroku=0;
+
+// This is an example of an exported function.
+DLL_SUGOROKU_API int fnDLL_Sugoroku(void)
+{
+    return 42;
+}
+
+// This is the constructor of a class that has been exported.
+// see DLL_Sugoroku.h for the class definition
+CDLL_Sugoroku::CDLL_Sugoroku()
+{
+    return;
+}
+
+int CDLL_Sugoroku::Add(int a, int b)
+{
+	return a + b;
+}
